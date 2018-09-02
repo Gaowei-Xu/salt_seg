bae4210a2fa711890fb175450d79ff8eb79f3e09
陈彬彬 <chenbinbin@chenbinbindeMacBook-Pro.local>
2018-09-02 22:31:59 +0800

    add test case

--- src/rle.py
+++ src/rle.py
@@ -7,6 +7,8 @@
 # @File    : rle.py
 
 import numpy as np
+import os
+import cv2
 
 
 class RLEncoder(object):
@@ -27,7 +29,7 @@ class RLEncoder(object):
         :param format:
         :return: run length as an array or string (if format is True)
         """
-        bytes = img.reshape(img.shape[0] * img.shape[1], order=order)
+        bytes = image.reshape(image.shape[0] * image.shape[1], order=order)
         runs = []       # list of run lengths
         r = 0           # the current run length
         pos = 1         # count starts from 1 per WK
@@ -55,4 +57,38 @@ class RLEncoder(object):
             return z[:-1]
         else:
             return runs
+
+
+def unit_test():
+    """
+    test for run-length encoding algorithm
+    :return:
+    """
+    train_imgs_root_dir = '../dataset/train/images/'
+    train_masks_root_dir = '../dataset/train/masks/'
+
+    train_csv_file_full_path = '../dataset/train.csv'
+
+    rf = open(train_csv_file_full_path, 'rb')
+    lines = rf.readlines() - 1  # skip first row
+    amount = len(lines)
+    codec = RLEncoder()
+
+    for i, line in enumerate(lines):
+        if 'id,rle_mask' in line:
+            continue
+        img_name = line.split(',')[0] + '.png'
+        rle_gt = line.split(',')[1].strip()
+        print 'Processing image {} ({}/{})...'.format(img_name, i, amount)
+
+        image_full_path = os.path.join(train_imgs_root_dir, img_name)
+        mask_full_path = os.path.join(train_masks_root_dir, img_name)
+        image = cv2.imread(image_full_path, cv2.IMREAD_GRAYSCALE)
+        mask = cv2.imread(mask_full_path, cv2.IMREAD_GRAYSCALE)
+
+        result = codec.encode(mask)
+
+        print result
+
+
 
